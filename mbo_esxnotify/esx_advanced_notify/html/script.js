cd1f6ad854a6ab99c1870d994ea3620ee9bdbce0
Made By Owen <Owencrcr@gmail.com>
2025-05-13 06:22:40 +0100

    script.js

--- mbo_esxnotify/esx_advanced_notify/html/script.js
+++ mbo_esxnotify/esx_advanced_notify/html/script.js
@@ -1,24 +1,20 @@
 window.addEventListener('message', function (event) {
     if (event.data.action === 'notify') {
-        createNotification(event.data);
+        showNotification(event.data);
     }
 });
 
-function createNotification(data) {
-    const container = document.getElementById("notifications");
+function showNotification(data) {
+    const container = document.getElementById('notifications');
 
-    const notification = document.createElement("div");
-    notification.className = `notification ${data.type}`;
-    notification.innerHTML = `<strong>${data.title}</strong><br>${data.message}`;
+    const note = document.createElement('div');
+    note.className = `notification ${data.type}`;
+    note.innerHTML = `<strong>${data.title}</strong><br>${data.message}`;
 
-    container.appendChild(notification);
+    container.appendChild(note);
 
     setTimeout(() => {
-        notification.remove();
-        fetch(`https://${GetParentResourceName()}/notification_done`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-            body: JSON.stringify({})
-        });
+        note.remove();
     }, data.timeout || 5000);
 }
+
