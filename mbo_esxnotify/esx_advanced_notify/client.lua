38a49bb7fc897427950b89861b644c034335d7a1
Made By Owen <Owencrcr@gmail.com>
2025-05-13 06:20:48 +0100

    client.lua

--- mbo_esxnotify/esx_advanced_notify/client.lua
+++ mbo_esxnotify/esx_advanced_notify/client.lua
@@ -1,25 +1,37 @@
-RegisterNetEvent('esx_advanced_notify:send')
-AddEventHandler('esx_advanced_notify:send', function(data)
+RegisterNetEvent('esx_custom_notify:show')
+AddEventHandler('esx_custom_notify:show', function(data)
     SendNUIMessage({
-        action = "notify",
-        type = data.type or "info",
-        title = data.title or "Notification",
-        message = data.message or "",
+        action = 'notify',
+        type = data.type or 'info',
+        title = data.title or 'Notification',
+        message = data.message or '',
         timeout = data.timeout or 5000
     })
 end)
 
--- Example usage command (for testing)
+-- Exported function for ESX-style use
+exports('ShowNotification', function(msg)
+    TriggerEvent('esx_custom_notify:show', {
+        type = 'info',
+        title = 'Notification',
+        message = msg,
+        timeout = 5000
+    })
+end)
+
+-- Replace esx:showNotification for compatibility
+RegisterNetEvent('esx:showNotification')
+AddEventHandler('esx:showNotification', function(msg)
+    exports['esx_custom_notify']:ShowNotification(msg)
+end)
+
+-- Test command
 RegisterCommand("testnotify", function()
-    TriggerEvent('esx_advanced_notify:send', {
+    TriggerEvent('esx_custom_notify:show', {
         type = "success",
-        title = "Test Notification",
+        title = "MBO Test",
         message = "This is a test notification",
         timeout = 5000
     })
 end, false)
 
--- NUI focus management
-RegisterNUICallback("notification_done", function(data, cb)
-    cb("ok")
-end)
